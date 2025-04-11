bfa7fcf0ec82649eb1aa0c07439dbcb843670ce5
Tane-bit <it_oezkan.taneli@hotmail.com>
2025-04-11 11:32:41 +0200

    update

---
+++ script.js
@@ -0,0 +1,32 @@
+// Header Shrink on Scroll
+window.addEventListener("scroll", function() {
+    var header = document.getElementById("header");
+    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+    // Header Shrink Effekt bei Scrollen
+    if (window.scrollY > 150) {
+        header.classList.add("shrink");
+    } else {
+        header.classList.remove("shrink");
+    }
+
+    // Scroll-to-Top Button sichtbar machen
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        scrollToTopBtn.style.visibility = "visible";
+        scrollToTopBtn.style.opacity = "1";
+    } else {
+        scrollToTopBtn.style.visibility = "hidden";
+        scrollToTopBtn.style.opacity = "0";
+    }
+});
+
+// Füge die Funktion hinzu, um beim Klicken nach oben zu scrollen
+let scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+scrollToTopBtn.addEventListener("click", function(event) {
+    event.preventDefault(); // Verhindert das Standardverhalten des Links
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth" // Scrollen mit einer sanften Animation
+    });
+});
